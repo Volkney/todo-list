3f4d914f74ca3fb01fec84cbcfcc2a0dc9928bab
Jorge Tinoco <volkney@Jorges-MacBook-Air.local>
2023-04-24 17:27:32 -0500

    added css reset style sheet

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import _ from 'lodash';
+import './reset.css';
+import './style.css';
+import Pencil from './pencil.png';
+import Radio from './radio-btn.png';
 
-function component() {
-  const element = document.createElement('div');
-
-    // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
