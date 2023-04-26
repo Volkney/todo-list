29e6372614ec013d00e74ecaf6aed1e0b4811c8c
Jorge Tinoco <volkney@Jorges-MacBook-Air.local>
2023-04-26 14:55:24 -0500

    added todoIcon to the header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import _ from 'lodash';
 import './reset.css';
 import './style.css';
-import Pencil from './pencil.png';
+import Pencil from './todoIcon.svg';
 import Radio from './radio-btn.png';
-
+//get layout div
+const header = document.querySelector('.header');
+const body = document.querySelector('.main');
+const footer = document.querySelector('.footer');
+//get children of header
+const upperHeader = document.querySelector('.upper-header');
+const bottomHeader = document.querySelector('.bottom-header');
+//add logo svg to upper header
+const logoImage = new Image();
+logoImage.src = Pencil;
+const logo = document.createElement('div');
+logo.classList.add('logo');
+logo.appendChild(logoImage);
+upperHeader.appendChild(logo);
+const logoText = document.createElement('div');
+logo.classList.add('');
