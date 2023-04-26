79e7064a157cafd6eabdc60cf5f25ed1d4cf9ffe
Jorge Tinoco <volkney@Jorges-MacBook-Air.local>
2023-04-26 17:09:32 -0500

    added side menu text and divs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './reset.css';
 import './style.css';
 import Pencil from './todoIcon.svg';
 import Burger from './burgerBtn.svg';
+import AddBtn from './plus.svg';
 /* import Radio from './radio-btn.png'; */
 //get layout div
 const header = document.querySelector('.header');
@@ -29,3 +30,29 @@ bottomHeader.append(burgerBtn, todoText);
 //main children
 const sideMenu = document.createElement('div');
 sideMenu.classList.add('side-menu');
+const taskList = document.createElement('div');
+taskList.classList.add('task-list');
+//side menu content
+sideMenu.innerHTML = `<ul class='menu'>
+  <li class="all">All</li>
+  <li class="today">Today</li>
+  <li class="week">Week</li>
+  <li class="projects">Projects
+  </li>
+</ul>
+<div class='add-container'></div>`;
+const plusBtn = new Image();
+plusBtn.classList.add('plus-button');
+plusBtn.src = AddBtn;
+
+// append the plusBtn and the 'add-project' element to the side menu
+const addProject = sideMenu.querySelector('.add-container');
+const addProjectText = document.createElement('div');
+addProjectText.classList.add('add-project');
+addProjectText.textContent = 'add a project';
+addProject.append(plusBtn, addProjectText);
+
+sideMenu.appendChild(addProject);
+
+// append the side menu and the task list to the body
+body.append(sideMenu, taskList);
